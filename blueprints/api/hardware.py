2680f7e3f9d60c3031b277c10790e0e98fdbe0c4
Brett Carroll <brettwcarroll@hotmail.com>
2020-03-11 23:19:26 -0400

    use cpuinfo instead of RPi.version

--- blueprints/api/hardware.py
+++ blueprints/api/hardware.py
@@ -4,7 +4,8 @@ from flask import render_template
 from flask import abort
 from flask import jsonify
 from jinja2 import TemplateNotFound
-from RPi.version import memory,manufacturer,processor,type,model,version,revision,info
+#from RPi.version import memory,manufacturer,processor,type,model,version,revision,info
+from cpuinfo import get_cpu_info
 
 logger = logging.getLogger(__name__)
 
@@ -12,8 +13,8 @@ api_hardware = Blueprint('api_hardware', __name__, template_folder='templates')
 
 @api_hardware.route('/api/v1/hardware/')
 def api_get_hardware_info():
-    hardware_info = {'memory': memory,'manufacturer':manufacturer,'processor':processor,'type':type,'model':model,'version':version,'revision':revision,'info':info}
-    return jsonify(hardware_info)
+    hw = get_cpu_info()
+    return jsonify(hw)
 
 
 
